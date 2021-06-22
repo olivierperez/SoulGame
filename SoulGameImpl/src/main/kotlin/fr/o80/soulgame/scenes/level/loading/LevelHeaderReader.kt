b2ff2ed111862f5e1cfa60c50626301a357fb75f
Olivier PEREZ <olivier@olivierperez.fr>
2021-06-22 21:17:36 +0200

    Rewrite the checking of level's headers

--- SoulGameImpl/src/main/kotlin/fr/o80/soulgame/scenes/level/loading/LevelHeaderReader.kt
+++ SoulGameImpl/src/main/kotlin/fr/o80/soulgame/scenes/level/loading/LevelHeaderReader.kt
@@ -11,11 +11,11 @@ class LevelHeaderReader(
 ) {
 
     private var levelName: String? = null
-    private var manaGainAtPortal: Int? = null
-    private var manaGainAtConversion: Int? = null
-    private var manaInitial: Int? = null
-    private var manaLoss: Int? = null
-    private var manaMax: Int? = null
+    private var manaGainAtPortal: String? = null
+    private var manaGainAtConversion: String? = null
+    private var manaInitial: String? = null
+    private var manaLoss: String? = null
+    private var manaMax: String? = null
     private var font: String? = null
     private var spriteCharacters: String? = null
     private var spriteDoors: String? = null
@@ -26,11 +26,11 @@ class LevelHeaderReader(
         val parts = line.trim().split('=', limit = 2)
         when (parts[0]) {
             "Level.Name" -> levelName = parts[1]
-            "Mana.GainAtPortal" -> manaGainAtPortal = parts[1].toInt()
-            "Mana.GainAtConversion" -> manaGainAtConversion = parts[1].toInt()
-            "Mana.Initial" -> manaInitial = parts[1].toInt()
-            "Mana.Loss" -> manaLoss = parts[1].toInt()
-            "Mana.Max" -> manaMax = parts[1].toInt()
+            "Mana.GainAtPortal" -> manaGainAtPortal = parts[1]
+            "Mana.GainAtConversion" -> manaGainAtConversion = parts[1]
+            "Mana.Initial" -> manaInitial = parts[1]
+            "Mana.Loss" -> manaLoss = parts[1]
+            "Mana.Max" -> manaMax = parts[1]
             "Font.Path" -> font = parts[1]
             "Sprite.Characters" -> spriteCharacters = parts[1]
             "Sprite.Doors" -> spriteDoors = parts[1]
@@ -43,21 +43,29 @@ class LevelHeaderReader(
     fun build(): LevelSettings {
         return LevelSettings(
             code = code,
-            name = levelName ?: throw MalformedLevelFile("Level.Name is not set!"),
+            name = requireString(levelName, "Level.Name"),
             mana = ManaConfig(
-                gainAtPortal = manaGainAtPortal ?: throw MalformedLevelFile("Mana.GainAtPortal is not set!"),
-                gainAtConversion = manaGainAtConversion ?: throw MalformedLevelFile("Mana.GainAtConversion is not set!"),
-                initial = manaInitial ?: throw MalformedLevelFile("Mana.Initial is not set!"),
-                loss = manaLoss ?: throw MalformedLevelFile("Mana.Loss is not set!"),
-                max = manaMax ?: throw MalformedLevelFile("Mana.Max is not set!"),
+                gainAtPortal = requireInt(manaGainAtPortal, "Mana.GainAtPortal"),
+                gainAtConversion = requireInt(manaGainAtConversion, "Mana.GainAtConversion"),
+                initial = requireInt(manaInitial, "Mana.Initial"),
+                loss = requireInt(manaLoss, "Mana.Loss"),
+                max = requireInt(manaMax, "Mana.Max"),
             ),
             font = font ?: throw MalformedLevelFile("Font.Path is not set!"),
             sprite = SpritesConfig(
-                characters = spriteCharacters ?: throw MalformedLevelFile("Sprite.Characters is not set!"),
-                doors = spriteDoors ?: throw MalformedLevelFile("Sprite.Doors is not set!"),
-                walls = spriteWalls ?: throw MalformedLevelFile("Sprite.Walls is not set!"),
+                characters = requireString(spriteCharacters, "Sprite.Characters"),
+                doors = requireString(spriteDoors, "Sprite.Doors"),
+                walls = requireString(spriteWalls, "Sprite.Walls"),
             ),
-            endWhen = conditionParser.parse(endWhen ?: throw MalformedLevelFile("EndWhen is not set!"))
+            endWhen = conditionParser.parse(requireString(endWhen, "EndWhen"))
         )
+    }
+
+    private fun requireString(value: String?, name: String): String {
+        return value ?: throw MalformedLevelFile("$name is not set!")
+    }
+
+    private fun requireInt(value: String?, name: String): Int {
+        return value?.toIntOrNull() ?: throw MalformedLevelFile("$name is not set, or not a number!")
     }
 }
